c22b4d59022cfbff26f1cd087720f6a59fec7414
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-08-16 11:33:00 +0500

    adding base sort algorithms 1.0 making radix_sort look better

--- sorting_algorithms/radix_sort.py
+++ sorting_algorithms/radix_sort.py
@@ -1,31 +1,25 @@
+import random
+import time
+
 
 def radix_sort(arr):
-    # Находим максимальное число и определяем количество разрядов
     max_num = max(arr)
     max_digits = len(str(max_num))
-
-    # Создаем 10 пустых корзин
     buckets = [[] for _ in range(10)]
-
-    # Проходимся по разрядам
     for digit in range(max_digits):
-        # Распределяем элементы по корзинам
         for num in arr:
-            # Получаем цифру разряда
             current_digit = (num // (10**digit)) % 10
-            # Помещаем элемент в соответствующую корзину
             buckets[current_digit].append(num)
-
-        # Собираем элементы из корзин в исходный массив
         arr = [num for bucket in buckets for num in bucket]
-        # Очищаем корзины
         buckets = [[] for _ in range(10)]
 
     return arr
 
 
-# Пример использования
-nums = [329, 457, 657, 839, 436, 720, 355]
-sorted_nums = radix_sort(nums)
-print(sorted_nums)
+array_radix_sort = [
+    random.randint(-10_000, 10_000) for _ in range(10_000)
+]
+start = time.time()
+radix_sort(array_radix_sort)
+print(time.time() - start)
 
