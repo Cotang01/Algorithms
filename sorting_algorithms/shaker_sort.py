68c9b019aef44b0e3e5d393948bb365a9cb36a5c
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-08-17 15:24:44 +0500

    fix shaker_sort.py

--- sorting_algorithms/shaker_sort.py
+++ sorting_algorithms/shaker_sort.py
@@ -1,10 +1,13 @@
 import random
 import time
 
-''' SOMEHOW DOESN'T WORK
-Complexity: -
-Compatibility with negative numbers: -
-Speed: -
+'''
+Complexity: 
+    Best = O(n)
+    Average = O(n^2)
+    Worst = O(n^2)
+Compatibility with negative numbers: Yes
+Speed: ~5.6s for 10_000-sized list
 '''
 
 
@@ -20,11 +23,11 @@ def shaker_sort(arr: list) -> list:
             if arr[i - 1] > arr[i]:
                 arr[i], arr[i - 1] = arr[i - 1], arr[i]
         left += 1
-        return arr
+    return arr
 
 
 array_shaker_sort = [
-    random.randint(-10_000, 10_000) for _ in range(1_000)
+    random.randint(-10_000, 10_000) for _ in range(10_000)
 ]
 print(array_shaker_sort)
 start = time.time()
