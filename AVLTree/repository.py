043b7e6cf6c50695af445802824cd7972733fcc3
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2023-08-22 21:05:29 +0500

    small visual change

--- AVLTree/repository.py
+++ AVLTree/repository.py
@@ -4,20 +4,20 @@ from avl_tree import AVLTree
 
 class AVLTreeRemote:
     def __init__(self):
-        self.AVLTree = AVLTree()
+        self.avl_tree = AVLTree()
 
     def insert_node(self, value: int):
-        self.AVLTree.insert(value)
+        self.avl_tree.insert(value)
 
     def insert_multiple(self, amount: int):
         for i in range(amount):
-            self.AVLTree.insert(random.randint(0, 100))
+            self.avl_tree.insert(random.randint(0, 100))
 
     def find_node(self, value: int):
-        print(self.AVLTree.find(value))
+        print(self.avl_tree.find(value))
 
     def delete_node(self, value: int):
-        self.AVLTree.delete(value)
+        self.avl_tree.delete(value)
 
     def print_tree(self):
-        print(self.AVLTree)
+        print(self.avl_tree)
